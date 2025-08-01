566ba635eed1b55bc2445d3e56d1646d0e445897
Ajay <ajayjadhav85200@gmail.com>
2025-08-01 12:20:42 +0530

    verma jwls 2 features added

--- script.js
+++ script.js
@@ -23,6 +23,14 @@ function navigateToScheme() {
   Toaster.postMessage("navigateScheme");
 }
 
+function navigateToCatalogues() {
+  Toaster.postMessage("navigateToCatalogues");
+}
+
+function navigateToQuickPurchase() {
+  Toaster.postMessage("navigateToQuickPurchase");
+}
+
 function navigateToBankDetails() {
   Toaster.postMessage("navigateBankDetails");
 }
@@ -138,11 +146,11 @@ const swiper = new Swiper(".brandingSwiper", {
         const arrow = document.getElementById("arrowIcon");
 
         if (!expanded) {
-          container.classList.remove("max-h-[13.5rem]");
+          container.classList.remove("max-h-[15.5rem]");
           arrow.classList.add("rotate-180");
           expanded = true;
         } else {
-          container.classList.add("max-h-[13.5rem]");
+          container.classList.add("max-h-[15.5rem]");
           arrow.classList.remove("rotate-180");
           expanded = false;
         }
